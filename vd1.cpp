0e3bd77af6d535b515e1396de9911e2e8aab565d
Quyền Nguyễn Nhất <nguyennhatquyenyinh@gmail.com>
2023-09-23 20:44:53 +0700

    modify example

---
+++ vd1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val = 5 ;
+    while (val >= 0)
+    {
+        cout << val << " ";
+        val = val - 1;
+    }
+    return 0;
+}
---
+++ vd2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5 ;
+    while (x >= 0)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
